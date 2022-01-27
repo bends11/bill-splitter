ec8df658d369ded6036b3652e4ab431faba41ff1
Ben Sweeney <bends11@vt.edu>
2022-01-27 17:20:50 -0500

    Add summary page to calculate totals

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { PersonListComponent } from './person-list/person-list.component';
 import { ItemListComponent } from './item-list/item-list.component';
 import { AddItemComponent } from './add-item/add-item.component';
+import { SummaryComponent } from './summary/summary.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/people', pathMatch: 'full' },
   { path: 'people', component: PersonListComponent },
   { path: 'items', component: ItemListComponent },
   { path: 'add-item', component: AddItemComponent },
+  { path: 'summary', component: SummaryComponent },
 ]
 
 
