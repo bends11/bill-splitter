40f559a226d1cf6e1c1a58af2fd20e0306a7b6fd
Ben Sweeney <bends11@vt.edu>
2022-04-01 21:29:57 -0400

    Update deployment script to include build

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ bucket="s3://$project.sweeney-project.com"
 backup=backup
 exclusions="--exclude=$backup/* --exclude=$project/*"
 
-
+ng build
 aws s3 rm "$bucket/$backup" --recursive
 aws s3 cp $bucket "$bucket/$backup" --recursive $exclusions
 aws s3 cp ./dist/ $bucket --recursive
