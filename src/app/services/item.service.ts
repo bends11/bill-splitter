186d88c15b95b97df148d8e6b4c1ef7bb796f623
Ben Sweeney <bends11@vt.edu>
2022-04-01 21:25:39 -0400

    Add itemized summary

--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -16,7 +16,35 @@ export class ItemService {
   private items: Map<string, Item> = new Map<string, Item>();
   private itemSubject: Subject<Map<string, Item>> = new BehaviorSubject<Map<string, Item>>(this.items);
 
-  constructor() { }
+  constructor() {
+    this.items.set('pizza', {
+      name: 'pizza',
+      price: 1,
+      people: ['Ben'],
+      quantity: 1,
+    });
+
+    this.items.set('hotdog', {
+      name: 'hotdog',
+      price: 2,
+      people: ['Jacob'],
+      quantity: 1,
+    });
+
+    this.items.set('salsa', {
+      name: 'salsa',
+      price: 3,
+      people: ['Ben'],
+      quantity: 1,
+    });
+
+    this.items.set('ice cream', {
+      name: 'ice cream',
+      price: 4,
+      people: ['Jacob'],
+      quantity: 1,
+    });
+  }
 
   get items$() {
     return this.itemSubject.asObservable();
