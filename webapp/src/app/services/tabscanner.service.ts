ccbb2c0eeebe795386e61faf5d6e0577ae33b2fc
Ben Sweeney <bends11@vt.edu>
2025-06-23 16:45:29 -0400

    Add environment variables and update deployment script

--- webapp/src/app/services/tabscanner.service.ts
+++ webapp/src/app/services/tabscanner.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TabscannerService {
 
-  private apiUrl: string = 'https://api.bill-spliiter.com/tabscanner';
+  private apiUrl: string = `${environment.webserviceBaseUrl}/tabscanner`;
 
   constructor(private http: HttpClient) { }
 
--- webapp/src/environments/environment.prod.ts
+++ webapp/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  webserviceBaseUrl: 'https://api.bill-spliiter.com'
 };
